c252c125ce42043f80fe57188213dff929a6cfd1
kangjiastar <168568@123.com>
2015-06-23 10:25:10 +0800

    Initial Commit

---
+++ swift - 01Tests/swift___01Tests.swift
@@ -0,0 +1,36 @@
+//
+//  swift___01Tests.swift
+//  swift - 01Tests
+//
+//  Created by apple on 15/6/23.
+//  Copyright (c) 2015年 cloud7. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class swift___01Tests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
