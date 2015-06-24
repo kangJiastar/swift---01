8372b1681391a70474141ec8794f13f06e2b9a2a
kangjiastar <168568@123.com>
2015-06-24 15:04:23 +0800

    To 160

--- swift - 01/ViewController.swift
+++ swift - 01/ViewController.swift
@@ -157,11 +157,127 @@ class ViewController: UIViewController {
         */
         //1.continue(结束本次循环) 告诉一个循环体立刻停止本次循环迭代,重新开始下次循环迭代。就好像在说“本 次循环迭代我已经执行完了”,但是并不会离开整个循环体。
         //2.￼break(结束循环,跳转到表示循环体结束的大括号(})后的第一行代码) 语句会立刻结束整个控制流的执行。当你想要更早的结束一个 switch 代码块或者一 个循环体时,你都可以使用 break 语句。
+        
+        
+        /**
+        函数
+        */
+        //1.函数参数的类型定义在变量名之后
+        func sayHello(personName:String) ->String{
+            return personName;
+        }
+        
+        println(sayHello("hello everyone"))
+        
+        func sayHelloEasy(){
+            println("hello everyBody")
+        }
+        
+        sayHelloEasy()
+        
+        //2.形参名称定义
+        //(1)外部形参名
+        func sayWhat(nothing no:String , everything every:String){
+            println(no + every)
+        }
+        sayWhat(nothing: "没有", everything: "一切")
+        
+        //(2)#
+        func sayWhatSecond(#nothing:String , #everything:String){
+        
+        }
+        
+        sayWhatSecond(nothing: "没有", everything: "所有")
+        
+        //3.可变形参
+        //注意:函数最多可以有一个可变形参,而且它必须出现在参数列表的最后,以避免使 用多个形参调用函数引发歧义。如果你的函数有一个或多个带有默认值的形参,并且还有可变形参,请将可变形参放在所有默认形参之后,也就是的列表的最末尾。
+        
+
+        //注:swift对于double和int区分的非常明确
+        /**问题:怎么把三个点去掉*/
+        func acerageDouble(#numbers:Double...) -> Double {
+            var sum = 0.0;
+            
+            for aver in numbers{
+                sum += aver;
+            }
+            
+            return sum;
+        }
+        
+        
+        println(acerageDouble(numbers: 1.0,2.0,3.0))
+        
+        //4.变量形参,在形参之前加var
+        //5.In-out 形参,传入地址,类似于C语言传入参数地址,进而达到修改可以返回的目的
+        //6.(Int, Int) -> Int 函数类型
+        var saySome: (String ,String) -> () = sayWhat
+        
+        saySome("123","456")
+        
+        //7.作为形参类型的函数类型
+        //8.作为返回类型的函数类型
+        
+        
+        /**
+        闭包
+        */
+        //1.闭包的函数体部分由关键字in引入.该关键字表示闭包的参数和返回值类型定义已经完成,闭包函数体即将开始。
+        
+//        闭包表达式语法有如下一般形式:
+//           {
+//            (parameters) -> returnType in statements
+//           }
+//        reversed = sort(names, { (s1: String, s2: String) -> Bool in return s1 > s2 } )
+//        reversed = sort(names, { s1, s2 in return s1 > s2 } ) <类型和返回值将被推断出来,不需要指明>
+//        reversed = sort(names, { s1, s2 in s1 > s2 } ) <单行表达式闭包可以省略 return>
+//        reversed = sort(names, { $0 > $1 } ) <Swift 自动为内联函数􏰀供了参数名称简写功能,您可以直接通过 $0,$1,$2 等名字来引用 的闭包的参数的值。对应参数名称简写的类型会通过函数类型进行推断, in 关键字也同样可以被省略>
+//        reversed = sort(names, >) < Swift 的 String 类型定义了关于大于号 (>) 的字符串实现,让其作为一个函数接受两个 String 类型的参数并返回 Bool 类型的值。而这正好与 sort 函数的第二个参数需要的函数类型相符合>
+        //2.Trailing 闭包
+//        reversed = sort(names) { $0 > $1 } <Trailing 闭包>
+        
+        
+        /**
+        枚举类型
+        */
+        //1.像 Swift 中其他类型一样,它们的名字(例如 CompassPoint 和 Planet)必须以一个大写字母开头。
+        //2.默认定义的枚举类型不是按照整形1.2.3...的顺序排布,而是单独的类型
+        
+        /**
+        类和结构体
+        */
+        //1.注意:在你每次定义一个新类或者结构体的时候,实际上你是有效地定义了一个新的 Swift 类型。因此请使用 UpperCamelCase 这种方式来命名(如 SomeClass 和 SomeStructure 等),以便符合标准 Swift 类型的大写命名风格(如 String,Int 和 Bool)。相反的,请使用 lowerCamelCase 这种方式为属性和方法命名(如 framerate 和 incrementCount),以便和类区分。
+        
+        struct Resulotion{
+            var width = 0
+            var height = 0
+        }
   
+        let someResulution = Resulotion(width: 20, height: 30)
+        
+        //2.等价于"===”表示两个类类型(class type)的常量或者变量引用同一个类实例。
+        
+        
+        println(someResulution.width)
+        
+        
+        class Person{
+            var age = 18
+            var height = 1.78
+            var mRes = Resulotion()
+        }
+        
+        let somePerson =  Person()
+        
+        println(somePerson.age)
+        
+        
     }
   
     override func touchesBegan(touches: NSSet, withEvent event: UIEvent) {
         println(__FUNCTION__)
+        
+        
         
     }
     
@@ -169,6 +285,7 @@ class ViewController: UIViewController {
         println(__FUNCTION__)
     }
     
+
 
 }
 
